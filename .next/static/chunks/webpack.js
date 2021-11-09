2bd2206d5e8be72b7915c3dd1a27ed322a4e9968
William Fu <williamfu@williams-mbp.lan>
2021-11-09 00:32:13 -0500

    Add profile image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88c5757a8e575460"; }
+/******/ 		__webpack_require__.h = function() { return "bc092e6631c4b3cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
       Hello, I&apos;m a full-stack developer. I am learning this!
       </Box>
       <Box display={{md:'flex'}}>
@@ -12,6 +12,23 @@ const Page = () => {
             William Fu
           </Heading>
           <p> About me: Software Developer, Lifter, Gamer </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{base:4, md:0}}
+          ml={{md:6}}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/william.jpg"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
