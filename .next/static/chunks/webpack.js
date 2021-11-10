ca56e3a1a0cc75d1e2dd787e64b6ee6920b8d60d
William Fu <williamfu@williams-mbp.lan>
2021-11-09 20:51:44 -0500

    Bio and portfolio section complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ea25ff7e7203816"; }
+/******/ 		__webpack_require__.h = function() { return "d3d2130c81ca31bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Button,
+  Container,
+  Box,
+  Heading,
+  Image,
+  Link,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -31,6 +44,72 @@ const Page = () => {
           />
         </Box>
       </Box>
+
+      <Section delay={.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>Hi! Wiliam is a Software Engineer I currently working
+          at M&T bank for the Real Time Payments team. I enjoy learning and
+          implementin next technologies and tech stacks to broaden
+          my horizen and skill sets. Hobbies include gaming (Valorant,
+          Old School RuneScape, New World, and Super
+          Smash Bros Melee). Occasionally will stream on {' '}
+          <NextLink href="https://www.twitch.tv/10gica1" passHref={true}>
+            <Link>
+              twitch
+            </Link>
+          </NextLink>
+          . His latest project is {' '}
+          <NextLink href="/works/project1">
+            <Link>
+              Project1
+            </Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon/>} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1997</BioYear>
+          Born and raised in Baton Rouge, LA.
+        </BioSection>
+        <BioSection>
+          <BioYear>2019</BioYear>
+          Graduated from Rhodes College with a BA in Computer Science
+        </BioSection>
+        <BioSection>
+          <BioYear>2019</BioYear>
+          Hired at M&T Bank in the Technical Development Program
+        </BioSection>
+        <BioSection>
+          <BioYear>2021 to Present</BioYear>
+          Completed Technical Development Program and hired as SE1 on the Real Time Payments Team
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+       <Heading as="h3" variant="section-title">
+         I love
+       </Heading>
+       <Paragraph>
+         Games, {' '}
+         <Link href="https://open.spotify.com/user/1218885276/" target="_blank">
+          Music (Check out my playlists!)
+         </Link>
+         , Pets, Working out, Soup, Eating, and Learning.
+       </Paragraph>
+     </Section>
     </Container>
   )
 }
