7f24482187df1ae8e9378293fb6bf3c28f2c8987
William Fu <william-fu@live.com>
2021-11-13 13:44:48 -0500

    add github links and fix theme for clicked navbar background being teal

--- components/navbar.js
+++ components/navbar.js
@@ -16,18 +16,20 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button.js'
+import { IoLogoGithub } from 'react-icons/io5'
 
-const LinkItem = ({ href, path, children }) => {
+
+const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
-  const inactiveColor = useColorModeValue ('grey200', 'whiteAlpha.900')
+  const inactiveColor = useColorModeValue('grey200', 'whiteAlpha.900')
   return (
     <NextLink href={href} passHref>
       <Link
         p={2}
         bg={active ? 'glassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
-        //_target={_target}
-        //{...props}
+        _target={_target}
+        {...props}
       >
         {children}
       </Link>
@@ -50,13 +52,14 @@ const Navbar = props => {
     >
       <Container
         display="flex"
-        p={2} maxW="container.md"
+        p={2}
+        maxW="container.md"
         wrap="wrap"
         align="center"
         justify="space-between"
       >
-        <Flex align = "center" mr={5}>
-          <Heading as="h1" size="lg" letterSpacing={'tigher'}>
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
             <Logo />
           </Heading>
         </Flex>
@@ -74,12 +77,24 @@ const Navbar = props => {
           <LinkItem href="/posts" path={path}>
             Posts
           </LinkItem>
+          <LinkItem
+            _target="_blank"
+            href="https://github.com/williamfu24/portfolio"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            <IoLogoGithub />
+            Source
+          </LinkItem>
         </Stack>
 
         <Box flex={1} align="right">
           <ThemeToggleButton />
           <Box ml={2} display={{base: 'inline-block', md:'none'}}>
-            <Menu>
+            <Menu isLazy id="navbar-menu">
               <MenuButton
                 as={IconButton}
                 icon={<HamburgerIcon/>}
@@ -96,6 +111,12 @@ const Navbar = props => {
                   <NextLink href="/posts" passHref>
                     <MenuItem as={Link}>Posts</MenuItem>
                   </NextLink>
+                  <MenuItem
+                    as={Link}
+                    href="https://github.com/williamfu24/portfolio"
+                  >
+                    View Source
+                  </MenuItem>
                 </MenuList>
             </Menu>
           </Box>
